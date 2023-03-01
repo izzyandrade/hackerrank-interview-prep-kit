3f4278eb232e218d6f2048396facf908ba8f72c7
Izzy Andrade <israelcarneiro97@gmail.com>
2023-02-28 21:20:36 -0300

    feat: solved CountingValleys challenge

--- src/WarmUpChallenges/CountingValleys.ts
+++ src/WarmUpChallenges/CountingValleys.ts
@@ -1,18 +1,18 @@
-let inputString = '';
-let inputLines: string[] = [];
+const inputLines: string[] = ['10', 'DUDDDUUDUU'];
 let currentLine = 0;
 
-process.stdin.on('data', (inputStdin: string): void => {
-  inputString += inputStdin;
-});
-
-process.stdin.on('end', (): void => {
-  inputLines = inputString.split('\n');
-  inputString = '';
-});
+const SEA_LEVEL = 0;
 
 function readLine(): string {
   return inputLines[currentLine++];
+}
+
+function calculateNewPosition(position: number, step: string) {
+  let newPosition = 0;
+  if (step === 'U') newPosition = position + 1;
+  if (step === 'D') newPosition = position - 1;
+  console.log(newPosition);
+  return newPosition;
 }
 
 /*
@@ -25,8 +25,21 @@ function readLine(): string {
  */
 // steps = [DDUUUUDD]
 function countingValleys(steps: number, path: string): number {
-  console.log('path', path);
-  return 0;
+  let valleys = 0;
+  let isInValley = false;
+  let position = SEA_LEVEL;
+  const pathArray = path.split('');
+
+  pathArray.forEach((step) => {
+    position = calculateNewPosition(position, step);
+    if (position === -1 && !isInValley) isInValley = true;
+    if (position === 0 && isInValley) {
+      isInValley = false;
+      valleys += 1;
+    }
+  });
+
+  return valleys;
 }
 
 export default function main() {
