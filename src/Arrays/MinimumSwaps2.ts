a15b1478ccbfe1488da64007eaee1a65c357da71
Izzy Andrade <israelcarneiro97@gmail.com>
2023-08-30 23:56:28 -0300

    feat: finished MinimumSwaps2 exercise

---
+++ src/Arrays/MinimumSwaps2.ts
@@ -0,0 +1,38 @@
+const inputArray = [7, 1, 3, 2, 4, 5, 6];
+
+// this fuction will run through the array and swap the chain of numbers until the current value in the iteration is in its target index
+function swapChain(arr: number[], index: number) {
+  let swapCount = 0;
+  const cpyArr = arr;
+  let current = arr[index];
+  let target = arr[current - 1];
+  while (current !== target) {
+    // swap current position with target position and increase swap count
+    cpyArr[index] = target;
+    cpyArr[current - 1] = current;
+
+    swapCount += 1;
+
+    // update current and target positions
+    current = target;
+    target = arr[current - 1];
+  }
+  return swapCount;
+}
+
+// Complete the minimumSwaps function below.
+function minimumSwaps(arr: number[]) {
+  let totalSwaps = 0;
+  arr.forEach((element, index) => {
+    totalSwaps += swapChain(arr, index);
+  });
+  return totalSwaps;
+}
+
+function main() {
+  const res = minimumSwaps(inputArray);
+
+  console.log(res);
+}
+
+main();
